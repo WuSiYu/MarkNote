4f1b86ea3df3585cc69add91c55a59ac1c20002b
WuSiYu <13466768195@sina.cn>
2015-07-14 15:17:56 +0800

    为openshift和cnpaas等平台更改存储目录

--- index.php
+++ index.php
@@ -16,13 +16,17 @@
 	define('MD5_SALT', 'faowifankjsnvlaiuwef2480rasdlkvj');			//加密记事本密码时, 所使用的盐, 请一定要修改为自己设置的
 	define('MARK_DOWN_TYPE', '<<<-- MarkDown Type Note -->>>');		//Markdown 格式的标记
 	define('NOTE_CONFIG_FILE', 'NoteConfig.php');					//MarkNote的配置文件(在使用文件方式时,自动生成)
-	define('NOTE_DATA', 'NoteData/');								//MarkNote的数据目录(在使用文件方式时,自动生成)
+	if( isset( $_SERVER['OPENSHIFT_DATA_DIR'] ) )					//MarkNote的数据目录(在使用文件方式时) (自动生成)
+		define('NOTE_DATA', $_SERVER['OPENSHIFT_DATA_DIR'].'NoteData/');
+	else
+		define('NOTE_DATA', 'NoteData/');
 	define('NOTE_PASSWD_FILE', NOTE_DATA . 'passwd.data');			//MarkNote的密码存储文件(在使用文件方式时,自动生成)
 	define('NOTE_USERS_FILE', NOTE_DATA . 'users.data');			//MarkNote的用户存储文件(在使用文件方式时,自动生成)
 
 	$rewrite_create_htaccess_file = true;	//是否创建.htaccess文件以尝试实现伪静态
 	$rewrite_use_better_url = true;			//是否使用伪静态后的URL(如 http://note.domain/记事本名),若环境不支持伪静态则不要开启
 	//======================================
+
 
 
 	function show_error_exit($output,$show_return=true){
