24c3d13a817ce84b5797b4784b5096f3d4c918cc
WuSiYu <wu.siyu@hotmail.com>
2016-08-20 22:26:36 +0800

    开始前端开发

--- edit.php
+++ edit.php
@@ -2,175 +2,96 @@
 	require_once dirname(__FILE__).'/include/user.php';
 	require_once dirname(__FILE__).'/include/note.php';
 
+	if(!hasLogin()){
+		echo 'Please login';
+		exit();
+	}
+
 	if( hasLogin() && isset($_POST['action']) && $_POST['action'] == 'getNotelist' ){
 		$theNotebooks = getUserNotebooks($USERNAME);
-		var_dump($theNotebooks);
+		// var_dump($theNotebooks);
 		foreach ($theNotebooks as $value) {
 			if(is_int($value)){
 				?>
-				<div>
-					<span id="p-notelist-item-<?php echo $value?>" class="p-notelist-item" onclick="loadNote(<?php echo $value?>);">ID : <?php echo $value; ?> TITLE : <?php echo getNoteTitle($value); ?></span> <span class="btn-notelist-item-del" onclick="delNote(<?php echo $value; ?>);">del</span>
+				<div id="div-notelist-item-<?php echo $value?>" class="div-notelist-item" onmouseover="showNoteDelIcon(this)" onmouseout="hideNoteDelIcon(this)">
+					<span class="span-notelist-item-left"></span><span class="span-notelist-item-text" onclick="loadNote(<?php echo $value?>);"><i class="fa fa-file-text" aria-hidden="true"></i><?php echo getNoteTitle($value); ?></span><i class="fa fa-times i-notelist-item-del" onclick="delNote(<?php echo $value; ?>);"></i>
 				</div>
 				<?php
 			}
 			if(is_array($value)){
 				?>
-				<div class="p-notelist-item" >NOTEBOOK : <?php echo $value[0]; ?> <span class="btn-notelist-item-del" onclick="delNotebook('<?php echo $value[0]; ?>');">del</span></div>
-				<?php
-				foreach ($value as $note) {
-					if(is_int($note)){
-						?>
-						<div>
-							<span id="p-notelist-item-<?php echo $note?>" class="p-notelist-item p-notelist-item-subnote" onclick="loadNote(<?php echo $note?>);">ID : <?php echo $note; ?> TITLE : <?php echo getNoteTitle($note); ?></span> <span class="btn-notelist-item-del" onclick="delNote(<?php echo $note; ?>);">del</span>
-						</div>
-						<?php
-					}
-				}
+				<div class="div-notelist-folder">
+					<i class="fa fa-angle-down fa-lg i-notelist-folder-arrow" aria-hidden="true"></i>
+					<div class="div-notelist-item notebook-opened" onmouseover="showNotebookDelIcon(this)" onmouseout="hideNotebookDelIcon(this)" onclick="toggleNotebook(this);"><span class="span-notelist-item-left"></span><span class="span-notelist-item-text"><i class="fa fa-book" aria-hidden="true"></i><?php echo $value[0]; ?></span><i class="fa fa-times i-notelist-item-del" onclick="delNotebook('<?php echo $value[0]; ?>');"></i></div>
+					<?php
+					foreach ($value as $note) {
+						if(is_int($note)){
+							?>
+							<div id="div-notelist-item-<?php echo $note?>" class="div-notelist-item div-notelist-item-subnote" onmouseover="showNoteDelIcon(this)" onmouseout="hideNoteDelIcon(this)">
+								<span class="span-notelist-item-left span-notelist-item-left-subnote"></span><span class="span-notelist-item-text" onclick="loadNote(<?php echo $note?>);"><i class="fa fa-file-text" aria-hidden="true"></i><?php echo getNoteTitle($note); ?></span><i class="fa fa-times i-notelist-item-del" onclick="delNote(<?php echo $note; ?>);"></i>
+							</div>
+							<?php
+						}
+					}?>
+					<div class="div-notelist-item div-notelist-item-subnote">
+						<span class="span-notelist-item-left span-notelist-item-left-subnote"></span><span class="span-notelist-item-text" onclick="newSubnote('<?php echo $value[0]; ?>');"><i class="fa fa-plus" aria-hidden="true"></i>New Note</span>
+					</div>
+				</div><?php
 			}
 		}
+		?>
+		<div class="div-notelist-item">
+			<span class="span-notelist-item-left"></span><span class="span-notelist-item-text" onclick="newNote();"><i class="fa fa-plus" aria-hidden="true"></i>New Note</span>
+		</div>
+		<div class="div-notelist-item">
+			<span class="span-notelist-item-left"></span><span class="span-notelist-item-text" onclick="newNotebook();"><i class="fa fa-plus" aria-hidden="true"></i>New Notebook</span>
+		</div>
+		<?php
 		exit();
 	}
 
-?>
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>MarkNote</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-edit.php
-<h1>MarkNote</h1>
-
-<?php if(hasLogin()): ?>
-	<style type="text/css">
-		.p-notelist-item-selected{
-			background-color: #000;
-			color: #fff;
-		}
-
-		.p-notelist-item-subnote{
-			margin-left: 30px;
-		}
-
-		.btn-notelist-item-del{
-			color: red;
-		}
-	</style>
 	<script src="//cdn.bootcss.com/jquery/2.2.0/jquery.min.js"></script>
-	<script type="text/javascript">
-		var NOTEID=0;
+	<script src="include/js/edit.js"></script>
+	<link href="//cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="include/css/edit.css">
 
-		function loadNotelist(){
-			$.post('edit.php',{
-				action:'getNotelist',
-			},
-			function(data,status){
-				$("#div-notelist").html(data);
-			});
-		}
+</head>
 
-		function loadNote(id){
-			// alert(id);
-			if(NOTEID){
-				if(NOTEID == id){
-					return 0;
-				}
-				$("#p-notelist-item-"+NOTEID).removeClass("p-notelist-item-selected");
-			}
-			NOTEID=id;
-			$("#p-notelist-item-"+NOTEID).addClass("p-notelist-item-selected");
-			$.post('include/note.php',{
-				action:'getNote',
-				id:id
-			},
-			function(data,status){
-				// alert('Status: ' + status + data );
-				$("#textarea-note").val(data);
-			});
-		}
+<body>
 
-		function saveNote(){
-			$.post('include/note.php',{
-				action:'saveNote',
-				id:NOTEID,
-				content:$("#textarea-note").val()
-			},
-			function(data,status){
-				// alert('Status: ' + status + data );
-			});
-		}
+	<div id='header'>
+		<h1 id='header-title'>MarkNote</h1>
+	</div>
 
-		function delNote(id){
-			$.post('include/note.php',{
-				action:'delNote',
-				id:id
-			},
-			function(data,status){
-				// alert('Status: ' + status + data );
-				loadNotelist();
-			});
-		}
 
-		function delNotebook(notebook){
-			$.post('include/note.php',{
-				action:'delNotebook',
-				notebook:notebook
-			},
-			function(data,status){
-				// alert('Status: ' + status + data );
-				loadNotelist();
-			});
-		}
+	<div id="content">
+	 	<div id="sidebar">
+			<div id="div-notelist">load</div>
+	 	</div>
+	 	<div id="editor">
+<!-- 
+			<input type="text" id="input-newnote"></input>
+			<button id="btn-newnote">new note</button><br/>
 
-		$(document).ready(function(){
-			loadNotelist();
+			<input type="text" id="input-newnotebook"></input>
+			<button id="btn-newnotebook">new notebook</button><br/>
 
-			$("#btn-newnote").click(function(){
-				$.post('include/note.php',{
-					action:'newNote',
-					title:$("#input-newnote").val()
-				},
-				function(data,status){
-					// alert('Status: ' + status + data );
-					loadNotelist();
-				});
-			});	
+			<input type="text" id="input-subnote-book"></input>
+			<input type="text" id="input-subnote-note"></input>
+			<button id="btn-subnote">new notebook->note</button><br/>
+ -->
+			<textarea id="textarea-note" style="width: 800px;height: 600px;background-color: #191919;color: #fff;border: 0;padding: 20px;resize: none;"></textarea>
+			<button id="button-savenote" onclick="saveNote();">save</button>
+		</div>
+	</div>
 
-			$("#btn-newnotebook").click(function(){
-				$.post('include/note.php',{
-					action:'newNotebook',
-					notebook:$("#input-newnotebook").val()
-				},
-				function(data,status){
-					// alert('Status: ' + status + data );
-					loadNotelist();
-				});
-			});	
-
-			$("#btn-subnote").click(function(){
-				$.post('include/note.php',{
-					action:'newSubnote',
-					notebook:$("#input-subnote-book").val(),
-					title:$("#input-subnote-note").val()
-				},
-				function(data,status){
-					// alert('Status: ' + status + data );
-					loadNotelist();
-				});
-			});
-		});
-
-	</script>
-
-	<input type="text" id="input-newnote"></input>
-	<button id="btn-newnote">new note</button><br/>
-
-	<input type="text" id="input-newnotebook"></input>
-	<button id="btn-newnotebook">new notebook</button><br/>
-
-	<input type="text" id="input-subnote-book"></input>
-	<input type="text" id="input-subnote-note"></input>
-	<button id="btn-subnote">new notebook->note</button><br/>
-
-	<div id="div-notelist">load</div>
-
-	<textarea id="textarea-note"></textarea>
-	<button id="button-savenote" onclick="saveNote();">save</button>
-	<button onclick="alert(NOTEID);">test</button>
-
-<?php endif; ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,8 +9,6 @@
 		exit();
 	}
 
-
-
 	require 'include/user.php';
 
 	if( isset($_GET['type']) ){
@@ -19,31 +17,31 @@
 		$type = 'home';
 	}
 
-	echo 'index.php<br/>type='.$type.'<br/>';
+	// echo 'index.php<br/>type='.$type.'<br/>';
 
 
 	if( $type == 'user' ){
-		echo 'load '.$type.' page ---> ';
+		// echo 'load '.$type.' page ---> ';
 		require 'user.php';
 	}
 
 	if( $type == 'notebook' ){
-		echo 'load '.$type.' page ---> ';
+		// echo 'load '.$type.' page ---> ';
 		require 'notebook.php';
 	}
 
 	if( $type == 'note' ){
-		echo 'load '.$type.' page ---> ';
+		// echo 'load '.$type.' page ---> ';
 		require 'note.php';
 	}
 
 	if( $type == 'home' ){
 		if(hasLogin()){
-			echo 'load '.$type.' page ---> ';
+			// echo 'load '.$type.' page ---> ';
 			require 'edit.php';
 		}
 		else{
-			echo 'load '.$type.' page ---> ';
+			// echo 'load '.$type.' page ---> ';
 			require 'login.php';
 		}
 	}
